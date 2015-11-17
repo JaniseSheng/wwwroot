56f48c912f7a02a7644e6031873e7845da5da6cd
JaniseSheng <shengxx@survey.best>
2015-11-17 21:53:16 +0800

    #1

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'yubocaihang';
+$cfg_dbuser = 'yubocaihang';
+$cfg_dbpwd = '5jf3rqz6';
+$cfg_dbprefix = 'yu_';
+$cfg_db_language = 'utf8';
+
+
+?>
